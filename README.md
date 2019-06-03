92f0806dd98648ab8ae4f79f7283dbeaec3c4ed8
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-06-03 21:04:44 +0900

    Add dark and light options

---
+++ README.md
@@ -0,0 +1,29 @@
+# @kamataryo/gh-ogp
+
+Draw simple OGP image optimized for GitHub repository
+
+# usage
+
+```shell
+$ cd path/to/your/Node.js/project
+$ npx @kamataryo/gh-ogp dark > dark_image.png
+$ npx @kamataryo/gh-ogp light > light_image.png
+```
+
+It takes some time to install dependencies when your execute `gh-ogp` at first time.
+You can install `gh-ogp` globally.
+
+# development
+
+```shell
+$ git clone git@github.com:kamataryo/gh-ogp.git
+$ cd gh-ogp
+$ yarn
+$ ./cli.js dark > test.png
+$ ./cli.js light > test.png
+```
+
+# Samples
+
+![ogp sample](./samples/01.png)
+![ogp sample](./samples/02.png)
--- cli.js
+++ cli.js
@@ -1,14 +1,36 @@
 #!/usr/bin/env node
+
+const Command = require('xcm').default
 const readPkgUp = require('read-pkg-up')
 const drawer = require('./lib/drawer')
 
-const main = async () => {
+const capitalize = ([head, ...rest]) => head.toUpperCase() + rest.join('')
+
+const handler = theme => async (_0, opts) => {
+  const options = Object.entries(opts)
+    .map(([key, value]) => [
+      key
+        .split('-')
+        .map((word, i) => (i !== 0 ? capitalize(word) : word))
+        .join(''),
+      value
+    ])
+    .reduce((prev, [key, value]) => ({ ...prev, [key]: value }), {})
   const {
     package: { name: title, description }
   } = await readPkgUp()
-  const buf = drawer({ title, description })
+
+  const buf = drawer({ theme, title, description })
   process.stdout.write(buf)
 }
 
-// go!
-main()
+const command = new Command('gh-ogp')
+  .sub('dark')
+  .action(handler('dark'))
+  .super()
+  .sub('light')
+  .action(handler('light'))
+  .super()
+  .parse()
+// .option('bg-color', { default: 'white' })
+// .option('fg-color', { default: 'black' })
